6c7ff01399455a2a12ce61c32d450e7a01aa8794
Grant Heffernan <heffergm@gmail.com>
2015-03-30 09:16:38 -0400

    - sanitize the 'name' element of the extracts hash - verify the bounding box   - is it an array?   - does it have four elements?   - is it a valid bbox?

--- recipes/_extract_slices.rb
+++ recipes/_extract_slices.rb
@@ -5,14 +5,14 @@
 
 require 'json'
 
+def sanitize(input)
+  input.gsub(/[^0-9A-z.\-]/, '_')
+end
+
 # if the json override file exists, read it and use that data
 if File.exist?(node[:osmpolygons][:extracts][:hash_from_file])
   data = JSON.parse(File.read(node[:osmpolygons][:extracts][:hash_from_file]))
   node.set[:osmpolygons][:extracts][:hash] = data
-end
-
-def sanitize(input)
-  input.gsub(/[^0-9A-z.\-]/, '_')
 end
 
 # generate configs for each extract in our hash
@@ -20,8 +20,29 @@ node[:osmpolygons][:extracts][:force][:slices] ? slice_action = :run : slice_act
 node[:osmpolygons][:extracts][:hash].map do |name, bbox|
   sanitized_name = sanitize(name)
 
+  # is the bbox an array?
   if bbox.is_a? Array
-    log "bbox for #{sanitized_name} is type array, continue"
+    log "passed: bbox for #{sanitized_name} is type array"
+    # if so, does it have 4 elements?
+    if bbox.count == 4
+      # if so, is it a valid bounding box?
+      log "passed: bbox for #{sanitized_name} has four elements"
+
+      left    = bbox[0].to_f
+      bottom  = bbox[1].to_f
+      right   = bbox[2].to_f
+      top     = bbox[3].to_f
+
+      if left >= right
+        Chef::Application.fatal!("left coordinate (#{left}) is >= right coordinate (#{right}) for bbox #{sanitized_name}! Aborting...", 1)
+      elsif bottom >= top
+        Chef::Application.fatal!("bottom coordinate (#{bottom}) is >= top coordinate (#{top}) for bbox #{sanitized_name}! Aborting...", 1)
+      else
+        log "passed: bbox for #{sanitized_name} is valid"
+      end
+    else
+      Chef::Application.fatal!("bbox for #{sanitized_name} does not have four elements! Aborting...", 1)
+    end
   else
     Chef::Application.fatal!("bbox for #{sanitized_name} is not an array! Aborting...", 1)
   end
