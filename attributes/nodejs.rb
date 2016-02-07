72d24a25512073f05649ba3e68e5939804398566
Grant Heffernan <heffergm@gmail.com>
2016-02-07 07:44:50 -0500

    fix nodejs attributes

--- attributes/nodejs.rb
+++ attributes/nodejs.rb
@@ -3,6 +3,6 @@
 # Attributes:: nodejs
 #
 
-default[:nodejs][:version]            = '0.10.36'
-default[:nodejs][:install_method]     = 'binary'
-default[:nodejs][:checksum_linux_x64] = '2bc13477684a9fe534bdc9d8f4a8caf6257a11953b57c42cad9b919ee259a0d5'
+default[:nodejs][:version]                        = '0.10.36'
+default[:nodejs][:install_method]                 = 'binary'
+default[:nodejs][:binary][:checksum][:linux_x64]  = '2bc13477684a9fe534bdc9d8f4a8caf6257a11953b57c42cad9b919ee259a0d5'
